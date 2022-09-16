3018c1b314cb395777e1cb68e8b2e59c73e6f98b
Alex Turaev <altex_76@mail.ru>
2022-09-16 21:36:00 +0300

    first commit

---
+++ src/main/java/com/homework2_5/hw_collection_set/HwCollectionSetApplication.java
@@ -0,0 +1,13 @@
+package com.homework2_5.hw_collection_set;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwCollectionSetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwCollectionSetApplication.class, args);
+	}
+
+}
